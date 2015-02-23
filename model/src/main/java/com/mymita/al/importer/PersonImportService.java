79ebf9d69c1c3bcba7a8f91d72477d7cc5b16686
Andreas Höhmann <andreas.hoehmann@mymita.com>
2015-02-23 07:29:17 +0100

    [add] chunked based import for persons

--- model/src/main/java/com/mymita/al/importer/PersonImportService.java
+++ model/src/main/java/com/mymita/al/importer/PersonImportService.java
@@ -13,9 +13,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.google.common.base.Charsets;
+import com.google.common.base.Function;
+import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import com.googlecode.jcsv.reader.CSVReader;
 import com.googlecode.jcsv.reader.internal.CSVReaderBuilder;
 import com.mymita.al.domain.Person;
@@ -25,12 +30,8 @@ import com.mymita.al.repository.PersonRepository;
 @Service
 public class PersonImportService implements ImportService<Person> {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(PersonImportService.class);
-
-  @Autowired
-  transient PersonRepository personRepository;
-
-  private Gender asGender(final String value) {
+  @Nullable
+  private static Gender asGender(final String value) {
     if (value == null || value.trim().isEmpty()) {
       return null;
     }
@@ -43,9 +44,37 @@ public class PersonImportService implements ImportService<Person> {
     return null;
   }
 
-  private void deletePersons() {
+  private static final Logger LOGGER = LoggerFactory.getLogger(PersonImportService.class);
+
+  private static final Function<String[], Person> TO_PERSON = new Function<String[], Person>() {
+
+    @Override
+    public Person apply(@Nullable final String[] data) {
+      final String code = data[0];
+      final String lastName = data[1];
+      final String firstName = data[2];
+      final String birthName = data[3];
+      final Gender gender = asGender(data[4]);
+      final String yearOfBirth = data[5];
+      final String yearOfDeath = data[6];
+      final String yearsOfLife = data[7];
+      final String description = data[8];
+      final String reference = data[9];
+      final String link = data[10];
+      return new Person().personCode(code).lastName(lastName).firstName(firstName).birthName(birthName).gender(gender)
+          .yearOfBirth(yearOfBirth).yearOfDeath(yearOfDeath).yearsOfLife(yearsOfLife).description(description).reference(reference)
+          .link(link);
+    }
+  };
+
+  @Autowired
+  transient PersonRepository personRepository;
+
+  @Transactional(propagation = Propagation.REQUIRES_NEW)
+  public void deletePersons() {
     LOGGER.debug("Delete all persons");
     personRepository.deleteAll();
+    LOGGER.debug("Deleted all persons");
   }
 
   @Override
@@ -60,39 +89,24 @@ public class PersonImportService implements ImportService<Person> {
     LOGGER.debug("Added '{}' persons successfully", persons.size());
   }
 
-  private Person importPerson(final int i, final int max, final Person person, final ImportListener<Person> importListener) {
-    try {
-      final Person importedPerson = personRepository.save(person);
-      if (importListener != null) {
-        importListener.progressImport(person, i, max);
-      }
-      return importedPerson;
-    } catch (final org.springframework.dao.DataIntegrityViolationException e) {
-      throw new org.springframework.dao.DataIntegrityViolationException(String.format("Can't import person '%s'", person), e);
-    }
-  }
-
-  private void importPersons(final List<String[]> persons, final ImportListener<Person> importListener) {
+  @Transactional(propagation = Propagation.REQUIRES_NEW)
+  public void importPersons(final List<String[]> persons, final ImportListener<Person> importListener) {
     final int max = persons.size();
     int i = 1;
-    LOGGER.debug("Start adding '{}' persons", max);
-    for (final String[] data : persons) {
-      final String code = data[0];
-      final String lastName = data[1];
-      final String firstName = data[2];
-      final String birthName = data[3];
-      final Gender gender = asGender(data[4]);
-      final String yearOfBirth = data[5];
-      final String yearOfDeath = data[6];
-      final String yearsOfLife = data[7];
-      final String description = data[8];
-      final String reference = data[9];
-      final String link = data[10];
-      importPerson(i, max,
-          new Person().personCode(code).lastName(lastName).firstName(firstName).birthName(birthName).gender(gender)
-          .yearOfBirth(yearOfBirth).yearOfDeath(yearOfDeath).yearsOfLife(yearsOfLife).description(description).reference(reference)
-          .link(link), importListener);
-      i++;
+    final int chunkSize = 1000;
+    LOGGER.debug("Start adding given '{}' persons in chunks with '{}' persons", max, chunkSize);
+    for (final List<String[]> data : Lists.partition(persons, chunkSize)) {
+      try {
+        final Iterable<Person> importedPersons = personRepository.save(FluentIterable.from(data).transform(TO_PERSON));
+        if (importListener != null) {
+          for (final Person importedPerson : importedPersons) {
+            importListener.progressImport(importedPerson, i, max);
+            i++;
+          }
+        }
+      } catch (final org.springframework.dao.DataIntegrityViolationException e) {
+        throw new org.springframework.dao.DataIntegrityViolationException(String.format("Can't import persons '%s'", data), e);
+      }
     }
   }
 
