aed9123af2feddb9abe628c2b91c0ff4b795afcc
Andreas Höhmann <andreas.hoehmann@mymita.com>
2015-12-23 12:45:08 +0100

    [update] springdata and/or hibernate update cause changed handling for "unique constraint errors" - until this update i got a DataIntegrityViolationException now i got a hibernate "null pointer exception" :-/ [add] also now is jta dependency required

--- model/src/test/java/com/mymita/al/repository/PersonRepositoryTest.java
+++ model/src/test/java/com/mymita/al/repository/PersonRepositoryTest.java
@@ -9,8 +9,7 @@ import org.hamcrest.Matchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
-import org.springframework.test.context.transaction.BeforeTransaction;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
 import com.google.common.collect.Iterables;
@@ -18,78 +17,87 @@ import com.mymita.al.domain.Person;
 import com.mymita.al.domain.Person.Gender;
 
 @ContextConfiguration(locations = { "classpath:/META-INF/spring/context-test.xml" })
-public class PersonRepositoryTest extends AbstractTransactionalTestNGSpringContextTests {
+public class PersonRepositoryTest extends AbstractTestNGSpringContextTests {
+
+  static void setupData(final PersonRepository personRepository) throws Exception {
+    personRepository.deleteAll();
+    personRepository.save(new Person().firstName("Andreas").lastName("Höhmann").birthName("Höhmann").gender(Gender.MALE).personCode("0001")
+        .yearOfBirth("1976"));
+    personRepository.save(new Person().firstName("Albert").lastName("Einstein").birthName("Einstein").gender(Gender.MALE).personCode("0002")
+        .yearOfBirth("1879").yearOfDeath("1955"));
+    assertThat(personRepository.count(), Matchers.is(2L));
+    assertThat(Iterables.getFirst(personRepository.findAll(), null).getLastName(), is("Höhmann"));
+  }
 
   @Autowired
   transient PersonRepository personRepository;
 
   @Test
   public void findByLastName() throws Exception {
+    setupData(personRepository);
     assertThat(personRepository.findAll(person.lastName.containsIgnoreCase("Höhmann")), Matchers.<Person> iterableWithSize(1));
   }
 
   @Test
   public void findByLastNameOrBirthNameAndYearOfBirthAndYearOfDeathNeg() throws Exception {
-    assertThat(personRepository.findAll(anyOf(person.lastName.containsIgnoreCase("Einstein"),
-        person.birthName.containsIgnoreCase("Einstein")).and(person.yearOfBirth.eq("1879")).and(person.yearOfDeath.eq("1900"))),
+    setupData(personRepository);
+    assertThat(
+        personRepository.findAll(anyOf(person.lastName.containsIgnoreCase("Einstein"), person.birthName.containsIgnoreCase("Einstein"))
+            .and(person.yearOfBirth.eq("1879")).and(person.yearOfDeath.eq("1900"))),
         Matchers.<Person> iterableWithSize(0));
   }
 
   @Test
   public void findByLastNameOrBirthNameAndYearOfBirthAndYearOfDeathPos() throws Exception {
-    assertThat(personRepository.findAll(anyOf(person.lastName.containsIgnoreCase("Einstein"),
-        person.birthName.containsIgnoreCase("Einstein")).and(person.yearOfBirth.eq("1879")).and(person.yearOfDeath.eq("1955"))),
+    setupData(personRepository);
+    assertThat(
+        personRepository.findAll(anyOf(person.lastName.containsIgnoreCase("Einstein"), person.birthName.containsIgnoreCase("Einstein"))
+            .and(person.yearOfBirth.eq("1879")).and(person.yearOfDeath.eq("1955"))),
         Matchers.<Person> iterableWithSize(1));
   }
 
   @Test
   public void findByLastNameOrBirthNameAndYearOfBirthNeg() throws Exception {
-    assertThat(
-        personRepository.findAll(anyOf(person.lastName.containsIgnoreCase("Höhmann"), person.birthName.containsIgnoreCase("Höhmann")).and(
-            person.yearOfBirth.eq("1900"))), Matchers.<Person> iterableWithSize(0));
+    setupData(personRepository);
+    assertThat(personRepository.findAll(anyOf(person.lastName.containsIgnoreCase("Höhmann"), person.birthName.containsIgnoreCase("Höhmann"))
+        .and(person.yearOfBirth.eq("1900"))), Matchers.<Person> iterableWithSize(0));
   }
 
   @Test
   public void findByLastNameOrBirthNameAndYearOfBirthPos() throws Exception {
-    assertThat(
-        personRepository.findAll(anyOf(person.lastName.containsIgnoreCase("Höhmann"), person.birthName.containsIgnoreCase("Höhmann")).and(
-            person.yearOfBirth.eq("1976"))), Matchers.<Person> iterableWithSize(1));
+    setupData(personRepository);
+    assertThat(personRepository.findAll(anyOf(person.lastName.containsIgnoreCase("Höhmann"), person.birthName.containsIgnoreCase("Höhmann"))
+        .and(person.yearOfBirth.eq("1976"))), Matchers.<Person> iterableWithSize(1));
   }
 
   @Test
   public void findByLastNameOrBirthNameAndYearOfDeathNeg() throws Exception {
-    assertThat(
-        personRepository.findAll(anyOf(person.lastName.containsIgnoreCase("Höhmann"), person.birthName.containsIgnoreCase("Höhmann")).and(
-            person.yearOfDeath.eq("1900"))), Matchers.<Person> iterableWithSize(0));
+    setupData(personRepository);
+    assertThat(personRepository.findAll(anyOf(person.lastName.containsIgnoreCase("Höhmann"), person.birthName.containsIgnoreCase("Höhmann"))
+        .and(person.yearOfDeath.eq("1900"))), Matchers.<Person> iterableWithSize(0));
   }
 
   @Test
   public void findByLastNameOrBirthNameAndYearOfDeathPos() throws Exception {
-    assertThat(personRepository.findAll(anyOf(person.lastName.containsIgnoreCase("Einstein"),
-        person.birthName.containsIgnoreCase("Einstein")).and(person.yearOfDeath.eq("1955"))), Matchers.<Person> iterableWithSize(1));
+    setupData(personRepository);
+    assertThat(
+        personRepository.findAll(anyOf(person.lastName.containsIgnoreCase("Einstein"), person.birthName.containsIgnoreCase("Einstein"))
+            .and(person.yearOfDeath.eq("1955"))),
+        Matchers.<Person> iterableWithSize(1));
   }
 
   @Test
   public void findByYearOfBirth() throws Exception {
+    setupData(personRepository);
     assertThat(personRepository.findAll(anyOf(person.yearOfBirth.eq("1976"))), Matchers.<Person> iterableWithSize(1));
     assertThat(personRepository.findAll(anyOf(person.yearOfBirth.eq("1955"))), Matchers.<Person> iterableWithSize(0));
   }
 
-  @BeforeTransaction
-  public void setupData() throws Exception {
-    deleteFromTables("person");
-    personRepository.save(new Person().firstName("Andreas").lastName("Höhmann").birthName("Höhmann").gender(Gender.MALE).personCode("0001")
-        .yearOfBirth("1976"));
-    personRepository.save(new Person().firstName("Albert").lastName("Einstein").birthName("Einstein").gender(Gender.MALE)
-        .personCode("0002").yearOfBirth("1879").yearOfDeath("1955"));
-    assertThat(personRepository.count(), Matchers.is(2L));
-    assertThat(Iterables.getFirst(personRepository.findAll(), null).getLastName(), is("Höhmann"));
-  }
-
-  @Test(expectedExceptions = { DataIntegrityViolationException.class }, expectedExceptionsMessageRegExp = ".*constraint \\[UNIQUE_PERSON_CODE\\].*")
+  @Test(expectedExceptions = {
+      DataIntegrityViolationException.class }, expectedExceptionsMessageRegExp = ".*constraint \\[UNIQUE_PERSON_CODE\\].*")
   public void unqiuePersonCode() throws Exception {
-    // same person code 0001
-    personRepository.save(new Person().firstName("Peter").lastName("Lustig").birthName("Lustig").gender(Gender.MALE).personCode("0001")
-        .yearOfBirth("1912"));
+    setupData(personRepository);
+    personRepository.save(
+        new Person().firstName("Peter").lastName("Lustig").birthName("Lustig").gender(Gender.MALE).personCode("0001").yearOfBirth("1912"));
   }
 }
